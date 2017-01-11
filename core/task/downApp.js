a08c58c1685070b6e149382b88e9487eb6ceb9b0
SmallBlackboard <469894183@qq.com>
2017-01-11 14:23:14 +0800

    在方法中追加捕获异常

--- core/task/downApp.js
+++ core/task/downApp.js
@@ -9,25 +9,28 @@ var _ = require('../../common/helper');
 var request = require('request');
 var REQUST = require("co-request");
 function  *getApp(cloneOptions){
+    try{
+        var demourl = cloneOptions.master+'/getAttachments/getApp/'+cloneOptions.attachmentId;
+        var resultData =yield REQUST.get({ url: demourl+'?checkmd5=true'});
 
-    var demourl = cloneOptions.master+'/getAttachments/getApp/'+cloneOptions.attachmentId;
-    var resultData =yield REQUST.get({ url: demourl+'?checkmd5=true'});
-
-    var data = resultData.body;
-    var dataForm = JSON.parse(data);
-    var checkmd5 = dataForm.checkmd5;
-    var appName = dataForm.fileName;
-    var appFilePath = path.join(__dirname, '../../','.temp', 'app',checkmd5);
-    if(!fs.existsSync(appFilePath)){
-        _.mkdir(appFilePath);
-        var downFile = path.join(appFilePath,appName);
-        var promise = new Promise(function (resolve, reject) {
-            var out = request(demourl).pipe(fs.createWriteStream(downFile));
-            out.on('finish', function () {
-                resolve("OK");
+        var data = resultData.body;
+        var dataForm = JSON.parse(data);
+        var checkmd5 = dataForm.checkmd5;
+        var appName = dataForm.fileName;
+        var appFilePath = path.join(__dirname, '../../','.temp', 'app',checkmd5);
+        if(!fs.existsSync(appFilePath)){
+            _.mkdir(appFilePath);
+            var downFile = path.join(appFilePath,appName);
+            var promise = new Promise(function (resolve, reject) {
+                var out = request(demourl).pipe(fs.createWriteStream(downFile));
+                out.on('finish', function () {
+                    resolve("OK");
+                });
             });
-        });
-        var resultStr = yield promise;
+            var resultStr = yield promise;
+        }
+    }catch (e){
+        console.log(e);
     }
 
 
